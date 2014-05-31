a8a9e978446f9d978a7ecd5cf82f69357df08e0d
unknown <mark.hagan@WIN-0N0BA1V47QI.hps.com>
2014-05-31 13:36:06 -0400

    initial commit (sorry for so many things :D)

---
+++ ShelterHelper/Default.aspx
@@ -0,0 +1,123 @@
+﻿<%@ Page Title="Adopt an Animal" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ShelterHelper._Default" %>
+
+<asp:Content runat="server" ID="FeaturedContent" ContentPlaceHolderID="FeaturedContent">
+    <section class="featured">
+        <div class="content-wrapper">
+            <hgroup class="title">
+                <h1>Louisville : <%: Title %></h1>
+            </hgroup>
+        </div>
+    </section>
+</asp:Content>
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <asp:UpdatePanel runat="server" ID="UpdateFilters">
+        <ContentTemplate>
+            <asp:DropDownList ID="ColorSelector" AutoPostBack="true" OnSelectedIndexChanged="FilterAnimalsNow" runat="server"></asp:DropDownList>
+            <asp:DropDownList ID="GenderDropdown" AutoPostBack="true" OnSelectedIndexChanged="FilterAnimalsNow" runat="server"></asp:DropDownList>
+            <asp:DropDownList ID="AnimalBreed" AutoPostBack="true" OnSelectedIndexChanged="FilterAnimalsNow" runat="server"></asp:DropDownList>
+            <asp:CheckBox AutoPostBack="true" OnCheckedChanged="FilterAnimalsNow" ID="AnimalFixed" runat="server" Text="Already Fixed" />
+        </ContentTemplate>
+    </asp:UpdatePanel>
+
+    <asp:UpdatePanel runat="server" ID="UpdateContent">
+        <ContentTemplate>
+            <asp:Label ID="ResultText" Text="Showing All Pets" runat="server" Font-Bold="true"></asp:Label>
+            <asp:Repeater ID="RepeatAnimals" OnItemDataBound="RepeatAnimals_ItemDataBound" runat="server">
+                <HeaderTemplate>
+                    <table class="table">
+                        <tr>
+                            <th>Admitted Date</th>
+                            <th>Type</th>
+                            <th>Sex</th>
+                            <th>Size</th>
+                            <th>Color</th>
+                            <th>Breed</th>
+                            <th>Name</th>
+                            <th>Age</th>
+                            <th>Adopt Me</th>
+                        </tr>
+                </HeaderTemplate>
+                <ItemTemplate>
+                    <tr>
+                        <td>
+                            <%# Eval("Intake_Date") %>
+                        </td>
+                        <td>
+                            <%# Eval("Animal_Type") %>
+                        </td>
+                        <td>
+                            <%# Eval("sex") %>
+                        </td>
+                        <td>
+                            <%# Eval("size") %>
+                        </td>
+                        <td>
+                            <%# Eval("primary_color") %>
+                        </td>
+                        <td>
+                            <%# Eval("breed") %>
+                        </td>
+                        <td>
+                            <%# Eval("name") %>
+                        </td>
+                        <td>
+                            <%# Eval("estimated_age") %>
+                        </td>
+                        <td>
+                            <asp:LinkButton ID="AdoptMeButton" Text="Adopt Me" runat="server" OnClick="AdoptMeButton_Click" />
+                        </td>
+                    </tr>
+                </ItemTemplate>
+                <FooterTemplate>
+                    </table>
+                </FooterTemplate>
+            </asp:Repeater>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+
+    <asp:UpdatePanel ID="UpdateScripts" runat="server">
+        <ContentTemplate>
+            <div class="modal fade" id="animalModal" tabindex="-1" role="dialog" aria-labelledby="adoptionLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                            <h4 class="modal-title" id="adoptionLabel">Adopt
+                                <asp:Label ID="AnimalName" runat="server"></asp:Label></h4>
+                        </div>
+                        <div class="modal-body">
+                            You are the human! Here is all the information you need to adopt this pet:
+                            <br />
+                            <br />
+                            <h4>Text Me The Address:</h4>
+                            <div class="row">
+                                <div class="col-xs-8">
+                                    <asp:TextBox ID="PhoneNumber" runat="server" placeholder="502-xxx-xxxx"></asp:TextBox>
+                                </div>
+                                <div class="col-xs-4">
+                                    <asp:Button ID="SendAddressButton" OnClick="SendAddressButton_Click" runat="server" Text="Send Address" />
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-4">
+                                    <strong>Address:</strong>
+                                </div>
+                                <div class="col-xs-8">
+                                    <asp:Label ID="AnimalAddress" runat="server"></asp:Label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-12">
+                                    <iframe width="425" height="350" frameborder="0" id="embeddedMap" scrolling="no" marginheight="0" marginwidth="0" src=""></iframe>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+</asp:Content>
---
+++ ShelterHelper/Default.aspx.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Twilio;
+
+namespace ShelterHelper
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                var inventory = GetInventory();
+
+                RepeatAnimals.DataSource = inventory;
+                RepeatAnimals.DataBind();
+
+                ColorSelector.DataSource = GetColors(inventory);
+                ColorSelector.DataBind();
+
+                List<string> gender = new List<string>();
+                gender.Add("Any");
+                gender.Add("Female");
+                gender.Add("Male");
+
+                GenderDropdown.DataSource = gender;
+                GenderDropdown.DataBind();
+
+                AnimalBreed.DataSource = GetBreed(inventory);
+                AnimalBreed.DataBind();
+            }
+        }
+
+        protected void SendAddressButton_Click(object sender, EventArgs e)
+        {
+            var address = ((Button)sender).CommandArgument;
+            var convertedAddress = "http://maps.google.com/?q=" + address.Replace(" ", "+");
+            var client = new TwilioRestClient("YOURS", "YOURS");
+        
+            client.SendSmsMessage("YOURS", PhoneNumber.Text.Replace("(", "").Replace(")", "").Replace("-", ""), "ShelterHelper: " + convertedAddress);
+            ScriptManager.RegisterClientScriptBlock(UpdateScripts, typeof(string), "showModal", "$('#animalModal').modal('hide'); alert('You are awesome, thank you!'); window.location=window.location;", true);
+        }
+
+        protected void FilterAnimalsNow(object sender, EventArgs e)
+        {
+            var inventory = GetInventory();
+            var filteredInventory = new List<AnimalInventory>();
+
+            foreach (var item in inventory)
+                filteredInventory.Add(item);
+
+            // breed filter
+            if (AnimalBreed.SelectedItem.Text != "Any")
+            {
+                foreach (var animal in inventory)
+                {
+                    if (animal.Breed.ToLower() != AnimalBreed.SelectedItem.Text.ToLower())
+                    {
+                        filteredInventory.RemoveAll(p => p.AnimalID == animal.AnimalID);
+                    }
+                }
+            }
+
+            // color filters
+            if (ColorSelector.SelectedItem.Text != "Any")
+            {
+                foreach (var animal in inventory)
+                {
+                    if (!animal.Primary_Color.ToLower().Contains(ColorSelector.SelectedItem.Text.ToLower())) {
+                        filteredInventory.RemoveAll(p => p.AnimalID == animal.AnimalID);
+                    }
+                }
+            }
+
+            // gender filter
+            if (GenderDropdown.SelectedItem.Text != "Any")
+            {
+                foreach (var animal in inventory)
+                {
+                    if (!animal.Sex.ToLower().Contains(GenderDropdown.SelectedItem.Text.ToLower())) {
+                        filteredInventory.RemoveAll(p => p.AnimalID == animal.AnimalID);
+                    }
+                }
+            }
+
+            if (AnimalFixed.Checked)
+            {
+                foreach (var animal in inventory)
+                {
+                    if (animal.Sex.ToLower().Contains("spayed") || animal.Sex.ToLower().Contains("neutered"))
+                    {
+                        // do nothing
+                    }
+                    else
+                    {
+                        filteredInventory.RemoveAll(p => p.AnimalID == animal.AnimalID);
+                    }
+                }
+            }
+
+            RepeatAnimals.DataSource = filteredInventory;
+            RepeatAnimals.DataBind();
+
+            ResultText.Text = "Showing " + filteredInventory.Count.ToString() + " Pet(s).";
+        }
+
+        private List<AnimalInventory> GetInventory()
+        {
+            List<AnimalInventory> inventory = new List<AnimalInventory>();
+
+            using (var db = new AwesomePetsDataContext())
+            {
+                inventory = db.AnimalInventories.Where(p => p.Kennel_Status == "available").OrderBy(p => p.Intake_Date).ToList();
+            }
+
+            return inventory;
+        }
+
+        private List<string> GetColors(List<AnimalInventory> inventory)
+        {
+            List<string> result = new List<string>();
+
+            result.Add("Any");
+
+            foreach (var animal in inventory.OrderBy(p => p.Primary_Color))
+            {
+                if (!string.IsNullOrEmpty(animal.Primary_Color.Trim()) && !result.Contains(animal.Primary_Color.ToLower()))
+                {
+                    result.Add(animal.Primary_Color.ToLower());
+                }
+            }
+
+            return result;
+        }
+
+        private List<string> GetBreed(List<AnimalInventory> inventory)
+        {
+            List<string> result = new List<string>();
+
+            result.Add("Any");
+
+            foreach (var animal in inventory.OrderBy(p => p.Breed))
+            {
+                if (!string.IsNullOrEmpty(animal.Breed.Trim()) && !result.Contains(animal.Breed.ToLower()))
+                {
+                    result.Add(animal.Breed.ToLower());
+                }
+            }
+
+            return result;
+        }
+
+        protected void AdoptMeButton_Click(object sender, EventArgs e)
+        {
+            var id = ((LinkButton)sender).CommandArgument;
+            var animal = GetInventory().Where(p => p.AnimalID == id).FirstOrDefault();
+
+            AnimalName.Text = animal.Name;
+            AnimalAddress.Text = animal.Kennel_Number;
+            SendAddressButton.CommandArgument = animal.Kennel_Number + " Louisville, KY";
+
+            string addressUrl = "https://www.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=" + animal.Kennel_Number.Replace(" ", "+") + "+louisville+ky&output=embed";
+            
+            ScriptManager.RegisterClientScriptBlock(UpdateScripts, typeof(string), "showModal", "$('#animalModal').modal('show'); $('#embeddedMap').attr('src', '" + addressUrl + "');", true);
+            // adopt the mofo
+        }
+
+        protected void RepeatAnimals_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                var dataSource = e.Item.DataItem as AnimalInventory;
+
+                LinkButton adoptMeButton = e.Item.FindControl("AdoptMeButton") as LinkButton;
+
+                adoptMeButton.CommandArgument = dataSource.AnimalID;
+            }
+        }
+    }
+}
